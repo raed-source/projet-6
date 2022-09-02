0d984851bf8ce9a7cc8561cfc54eab0172793445
raed ABBAS <raed.abbas@hotmail.fr>
2022-09-02 23:40:55 +0200

    initialisation , server, et app js

---
+++ back/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+// créer une route
+const userRoute=require('./routes/userRoute');
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+  
+  app.use('/api/auth',userRoute)
+
+module.exports = app;
